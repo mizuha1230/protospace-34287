8c6910baa7c4d35b900d20afb4122d053992dd04
mizuha1230 <m63103284@gmail.com>
2021-04-10 17:11:25 +0900

    prototype.html.erbの編集

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -50,4 +50,5 @@ class PrototypesController < ApplicationController
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
+  
 end
